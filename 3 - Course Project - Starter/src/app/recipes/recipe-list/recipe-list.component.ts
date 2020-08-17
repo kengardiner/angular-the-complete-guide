dc0b1594ee3bce99f075fcee653e418a66091dca
Ken Gardiner <kenneth.gardiner@libertymutual.com>
2020-08-17 12:04:38 -0400

    More progress on main course project

--- 3 - Course Project - Starter/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 3 - Course Project - Starter/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,6 +7,7 @@ import {Recipe} from "../recipe.model";
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recipeWasSelected = new EventEmitter<Recipe>()
     recipes: Recipe[] = [
         new Recipe('A test recipe', 'This is a test', 'https://p0.pikist.com/photos/801/880/cookbook-recipes-food-cook-book-paper-hand-culinary-ingredients.jpg')
     ];
@@ -15,6 +16,10 @@ export class RecipeListComponent implements OnInit {
     }
 
     ngOnInit(): void {
+    }
+
+    onRecipeSelected (recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
     }
 
 }
