dea7c7d7aa127e8e174eefd1c7e2de2f8cce4c6c
Ken Gardiner <kenneth.gardiner@libertymutual.com>
2020-08-18 16:13:50 -0400

    More progress on main course project

--- 3 - Course Project - Starter/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 3 - Course Project - Starter/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import {Recipe} from "../recipe.model";
 export class RecipeListComponent implements OnInit {
     @Output() recipeWasSelected = new EventEmitter<Recipe>()
     recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is a test', 'https://p0.pikist.com/photos/801/880/cookbook-recipes-food-cook-book-paper-hand-culinary-ingredients.jpg')
+        new Recipe('A test recipe', 'This is a test', 'https://p0.pikist.com/photos/801/880/cookbook-recipes-food-cook-book-paper-hand-culinary-ingredients.jpg'),
+        new Recipe('A second test recipe', 'This is a second test', 'https://p0.pikist.com/photos/801/880/cookbook-recipes-food-cook-book-paper-hand-culinary-ingredients.jpg'),
     ];
 
     constructor() {
